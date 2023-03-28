678ec9b12a0a4b7e04b425a27d9b85f5546f361a
tarequemusa <tareque.web@gmail.com>
2023-03-28 12:23:17 +0600

    show data from cart

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -9,9 +9,16 @@ const Cart = ({cart}) => {
 
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart) {
-        totalPrice = totalPrice + product.price;
+        /*  if(product.quantity === 0) {
+             product.quantity = 1;
+         } */
+        // product.quantity = product.quantity || 1;
+
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
 
     const tax = totalPrice * 7 / 100;
@@ -20,8 +27,8 @@ const Cart = ({cart}) => {
 
     return (
         <div className='cart'>
-            <h4>Order Summary: </h4>
-            <p>Selected Items: {cart.length}</p>
+            <h4>Order Summary </h4>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping: ${totalShipping}</p>
             <p>Tax: {tax.toFixed(2)}</p>
