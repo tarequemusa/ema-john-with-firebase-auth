2f111b51c8e5529d186abc627beab8d62e872276
tarequemusa <tareque.web@gmail.com>
2023-04-05 12:24:51 +0600

    Implemented delete from cart

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h3>Login Coming Soon</h3>
+        </div>
+    );
+};
+
+export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,46 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Shop from './Components/Shop/Shop';
+import Home from './Components/Layout/Home';
+import Orders from './Components/Orders/Orders';
+import Inventory from './Components/Inventory/Inventory';
+import Login from './Components/Login/Login';
+import cartProductsLoader from './Loaders/cartProductsLoader';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/',
+        element: <Shop />,
+      },
+      {
+        path: 'orders',
+        element: <Orders></Orders>,
+        loader: cartProductsLoader
+      },
+      {
+        path: 'inventory',
+        element: <Inventory></Inventory>,
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      }
+    ],
+  }
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
